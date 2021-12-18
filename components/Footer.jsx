db9ab40ba4d385964c5f30a83f065647ebdae424
naveen <kalanaveen55@gmail.com>
2021-12-18 15:59:34 +0530

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className='bg-white mx-2'>
+            <p className='text-center text-xs font-medium p-2 sm:text-sm'>Copyright © 2022.Modern Blog
+                -All Rights Reserved.</p>
+        </div>
+    )
+}
+export default Footer;
